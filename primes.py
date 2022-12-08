8cdd78afffe366799d60eb1be7704845e6106502
YahyaKhan001 <102700231+YahyaKhan001@users.noreply.github.com>
2022-12-08 19:28:07 +0000

    Update primes.py

--- primes.py
+++ primes.py
@@ -1,6 +1,18 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
-
-def primes(number_of_primes):
-    list = []
-    return list
+def Prime(n):
+    for a in range(2, b // 2 + 1):
+        if b % a == 0:
+            return 0
+    return 1
+N = int(input("Enter the value of N:"))
+a = 2
+prime_numbers = []
+while 1:
+    if Prime(a):
+        prime_numbers.append(a)
+        if len(prime_numbers) == N:
+            break
+    a += 1
+print("First " + str(N) + " Prime numbers are:", end="")
+print(*lst)
